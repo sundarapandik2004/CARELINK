9eb1332753e6c5494c2093e2818801a55be1fa33
sundarapandik2004 <sundarapandi.k2004@gmail.com>
2026-05-12 15:35:02 +0530

    Update MyApplication.java

--- app/src/main/java/com/example/accessease/MyApplication.java
+++ app/src/main/java/com/example/accessease/MyApplication.java
@@ -19,9 +19,9 @@ public class MyApplication extends Application {
             try { MediaManager.get(); return; } catch (IllegalStateException ignored) {}
 
             Map<String, String> config = new HashMap<>();
-            config.put("cloud_name", "drzacgnfl");  // ← replace
-            config.put("api_key",    "813749947439217");      // ← replace
-            config.put("api_secret", "yREp_Zbsl-rQdHSzC5s2D8yvZYA");  // ← replace
+            config.put("cloud_name", "");  // ← replace
+            config.put("api_key",    "");      // ← replace
+            config.put("api_secret", "");  // ← replace
             MediaManager.init(this, config);
         } catch (Exception e) {
             android.util.Log.e("MyApplication",
